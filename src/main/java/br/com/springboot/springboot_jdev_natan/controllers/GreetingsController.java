48530dee28893c4f1ff77d513a81c42982027d7a
Natan Dadalt <natan.db@gmail.com>
2023-03-31 21:52:02 -0300

    Criando END-POINT lista todos

--- src/main/java/br/com/springboot/springboot_jdev_natan/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/springboot_jdev_natan/controllers/GreetingsController.java
@@ -1,24 +1,39 @@
 package br.com.springboot.springboot_jdev_natan.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.springboot_jdev_natan.model.Usuario;
 import br.com.springboot.springboot_jdev_natan.repository.UsuarioRepository;
 
 @RestController
 public class GreetingsController {
-   
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+	@GetMapping(value = "listatodos")
+	@ResponseBody /* Retorna os dados para o corpo da resposta */
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+		
+		List<Usuario> usuarios = usuarioRepository.findAll(); /*executa a consulta no BD*/
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/* Retorna a lista em JSON */
+	}
 }
