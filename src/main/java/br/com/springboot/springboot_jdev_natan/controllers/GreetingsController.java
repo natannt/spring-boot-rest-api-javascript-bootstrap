03b7573d6bcb2bb99d673307d33323a0d2609eb1
Natan Dadalt <natan.db@gmail.com>
2023-04-04 22:42:49 -0300

    Salvando com AJAX incompleto

--- src/main/java/br/com/springboot/springboot_jdev_natan/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/springboot_jdev_natan/controllers/GreetingsController.java
@@ -33,7 +33,7 @@ public class GreetingsController {
 	}
 
 	@PostMapping(value = "salvar") /*Mapeia a URL*/
-	@ResponseBody /*Descrição da resposta*/
+	@ResponseBody/*Descrição da resposta*/
 	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /* Recebe os dados para salvar*/
 		
 		Usuario user = usuarioRepository.save(usuario);
